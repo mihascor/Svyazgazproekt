c551eeecd3f64e441731dd3847dfd8f9fa9d3db9
mihascor <mihascorkiz@gmail.com>
2025-12-18 15:23:39 +0300

    Градиент

--- app/page.tsx
+++ app/page.tsx
@@ -230,7 +230,7 @@ export default function Home() {
               opacity: '0.4'
             }}>
           </div>
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+          {/*<div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>*/}
         </div>
         
         <div className="relative z-10 max-w-7xl mx-auto px-4 lg:px-8 text-center">
