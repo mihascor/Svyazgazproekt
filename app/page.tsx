d1bb096266c81a2c193fd50e8f4cec584287ed60
mihascor <mihascorkiz@gmail.com>
2025-12-18 14:53:06 +0300

    Фон №1

--- app/page.tsx
+++ app/page.tsx
@@ -224,7 +224,7 @@ export default function Home() {
           <div
             className="absolute inset-0"
             style={{
-              backgroundImage: `url('/app_fon.jpg')`,
+              backgroundImage: `url('/app_fon_1.png')`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
               opacity: '0.4'
