ececcc7acbb301f7d8d6f5d7a58285571bbd5868
mihascor <mihascorkiz@gmail.com>
2025-12-20 13:54:51 +0300

    Фон_реал проекты_без градиента

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -105,7 +105,7 @@ export default function ProjectsPage() {
                opacity: '0.3'
              }}>
         </div>
-        <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+        {/* <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div> */}
         
         <div className="relative max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
           <div className="text-center mb-12 md:mb-16">
