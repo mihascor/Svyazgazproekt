12a18c4c197be8e9196a3295047528d3c52812f6
mihascor <mihascorkiz@gmail.com>
2025-12-22 13:57:46 +0300

    Фон_Главная плашка меню_лого

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             <Link href="/" className="flex items-center space-x-6">
               <div className="relative">
                 <img 
-                  src="/logo_sgp.svg"
+                  src="/logo_sgp.png"
                   alt="СГП Связьгазпроект"
                   className="h-16 w-auto rounded-sm"
                 />
@@ -120,7 +120,7 @@ export default function Home() {
         <div className="flex items-center justify-between">
           <Link href="/" className="relative">
             <img 
-              src="/logo_sgp.svg"
+              src="/logo_sgp.png"
               alt="СГП Связьгазпроект"
               className="h-12 w-auto rounded-sm"
             />
