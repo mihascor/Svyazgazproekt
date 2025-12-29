9d08b4913f7f883033a31f578a5917a534198bac
mihascor <mihascorkiz@gmail.com>
2025-12-29 06:03:02 +0300

    О нас-фон-градиент-низ

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -124,7 +124,8 @@ export default function AboutPage() {
                  opacity: '0.8'
                }}>
           </div>
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+          {/* <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div> */}
+          <div className="absolute inset-x-0 bottom-0 h-16 bg-gradient-to-b from-transparent to-black pointer-events-none"></div>
         </div>
         
         <div className="relative z-10 max-w-6xl mx-auto px-4 lg:px-8">
